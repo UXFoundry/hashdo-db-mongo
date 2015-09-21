58903cdc03ecc31b3ebfe6f055fdca8a433ff1d5
Werner van Deventer <werner@brutaldev.com>
2015-09-21 18:23:32 +0200

    Migrated from HashDo project.

---
+++ README.md
@@ -0,0 +1,24 @@
+# \#Do MongoDB
+
+This is a database implementation plugin for persisting [\#Do](https://github.com/UXFoundry/hashdo) card state to [MongoDB](https://www.mongodb.org/) .
+
+## Getting Started
+TODO
+
+## MongoDB
+A local or remote instance of [MongoDB](https://www.mongodb.org/) is required. When connecting to the database, provide the appropriate connection string. If no connection string is provided, the it will try to connect to `mongodb://localhost/hashdo`
+
+## License
+Copyright 2015 (c). All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you
+may not use this file except in compliance with the License. You may
+obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing permissions
+and limitations under the License.
---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+  plugins = require('gulp-load-plugins')();
+  
+var BUILD_PATH = './build/';
+var JS_LOCATIONS = [
+  './index.js',
+  './models/*.js'
+];
+
+gulp.task('jshint', function () {
+  return gulp.src(JS_LOCATIONS)
+    .pipe(plugins.jshint('.jshintrc'))
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
+});
+
+// format JavaScript based on pretty rules
+gulp.task('pretty', function () {
+  return gulp.src(JS_LOCATIONS, {base: '.'})
+    .pipe(plugins.jsbeautifier({ config: '.jsbeautifyrc' }))
+    .pipe(gulp.dest('.'))
+});
+
+// bump version
+gulp.task('bump', function () {
+  return gulp.src('./package.json')
+    .pipe(plugins.bump({type: 'patch'}))
+    .pipe(gulp.dest('./'));
+});
+
+// del build
+gulp.task('clean', function (cb) {
+  require('del').bind(null, [BUILD_PATH]);
+  
+  return cb();
+});
+
+// build documentation
+gulp.task('docs', function () {
+  return gulp.src(JS_LOCATIONS.concat(['README.md']), {base: '.'})
+    .pipe(plugins.doxx({
+      title: 'HashDo MongoDB',
+      urlPrefix: ''
+    }))
+    .pipe(gulp.dest(BUILD_PATH + 'docs'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(JS_LOCATIONS, gulp.series('jshint'));
+});
+
+gulp.task('default', gulp.series('watch'));
